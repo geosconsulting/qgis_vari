6b1359be4ed3c0a6e00834d56f346aa1faf34200
geosconsulting <geos-consulting@fastwebnet.it>
2015-09-26 16:46:50 +0200

    COn finestra che chiede coord e script caratterische shapefile molto piu avanzato

--- content of shapefile.py
+++ content of shapefile.py
@@ -28,5 +28,46 @@ def analyze_shapefile():
     calculator.setSourceCrs(crs)
     calculator.setEllipsoid(crs.ellipsoidAcronym())
     calculator.setEllipsoidalMode(crs.geographicFlag())
+    
+    for feature in provider.getFeatures(QgsFeatureRequest()):
+        #cerca un campo con varianti di name
+        if "name" in attr_names:
+            feature_label = feature.attribute("name")
+        elif "Name" in attr_names:
+            feature_label = feature.attribute("name")
+        elif "NAME" in attr_names:
+            feature_label = feature.attribute("name")
+        else:            
+            feature_label = str(feature.id())
         
+        #che tipo di shape?
+        geometry = feature.geometry()
+        
+        if geometry.type() == QGis.Line:
+            length = int(calculator.measure(geometry)/1000)
+            tot_length = tot_length + length
+            feature_info ="line of length %d kilometers" % length
+        elif geometry.type() == QGis.Polygon:
+            area = int(calculator.measure(geometry)/1000000)
+            tot_area = tot_area + area
+            feature_info ="polygon of area %d square kilometers" % area
+        else:
+            geom_type = qgis.vectorGeometryType(geometry.type())
+            feature_info = "geometry of type %s" % geom_type
+        
+        QgsMessageLog.logMessage("%s: %s" % (feature_label,feature_info),tag="SPARC")
+    
+    #Status bar in basso sulla finestra principale...poco efficace
+    #iface.mainWindow().statusBar().showMessage("Finito di analizzare i records.....")
+    #iface.mainWindow().statusBar().clearMessage()
+        
+    #print "Total length of all line features: %d" % tot_length
+    #print "Total area of all polygon features: %d" % tot_area
+    
+    from qgis.gui import *
+    titolo = "Finito di analizzare i records.....\n"
+    messaggio = "Total length of all line features: %d \n" % tot_length
+    messaggio += "Total area of all polygon features: %d" % tot_area
+    iface.messageBar().pushMessage(titolo,messaggio, level = QgsMessageBar.INFO)
+    
 analyze_shapefile()
---
+++ finestra chiede lat lon.py
@@ -0,0 +1,42 @@
+from PyQt4.QtGui import *
+
+class MyDialog(QDialog):
+    
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setWindowTitle("Enter Coordinate")
+        
+        layout = QFormLayout(self)
+        
+        self.lat_label =QLabel("Latitude",self)
+        self.lat_field =QLineEdit(self)
+        
+        self.long_label =QLabel("Longitude",self)
+        self.long_field =QLineEdit(self)
+        
+        self.ok_btn =QPushButton("OK",self)
+        self.ok_btn.clicked.connect(self.accept)
+        
+        self.cancel_btn =QPushButton("Cancel",self)
+        self.ok_btn.clicked.connect(self.reject)
+        
+        btn_layout = QHBoxLayout(self)
+        btn_layout.addWidget(self.ok_btn)
+        btn_layout.addWidget(self.cancel_btn)
+        layout.addRow(self.lat_label, self.lat_field)
+        layout.addRow(self.long_label, self.long_field)
+        layout.addRow(btn_layout)
+        self.setLayout(layout)
+
+from qgis.gui import *
+
+dialog = MyDialog()
+if dialog.exec_() == QDialog.Accepted:
+    messaggio = "Ci Passo"
+    iface.messageBar().pushMessage(messaggio, level = QgsMessageBar.INFO)
+    lat = dialog.lat_field.text()
+    long = dialog.long_field.text()
+    print lat,long
+else:
+    messaggio = "Non Ci Passo"
+    iface.messageBar().pushMessage(messaggio, level = QgsMessageBar.INFO)
